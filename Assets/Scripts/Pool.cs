1a9176597abbef42e96359b78bc46a2e9394a604
Tom <t.rakoto76@gmail.com>
2020-07-06 11:06:56 +0200

    Pool and PoolableObject

---
+++ Assets/Scripts/Pool.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool : MonoBehaviour
+{
+    public List<PoolableObject> pool;
+
+    // ajoute n fois l'objet passé en parramètre dans la pool.
+    public void Initialize(PoolableObject obj, int n)
+    {
+        pool = Enumerable.Repeat(obj, n).ToList();
+    }
+
+    // prend le premier objet de la pool en la retirant
+    public PoolableObject getObject()
+    {
+        PoolableObject o = pool[0];
+        pool.RemoveAt(0);
+        return o;
+    }
+}
---
+++ Assets/Scripts/PoolableObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+public class PoolableObject : MonoBehaviour
+{
+   // To know if this object is used in game or inactive in pool.
+   public bool isActive = false ;
+   
+   public void Init()
+   {
+      gameObject.SetActive(true);
+   }
+   public void DeInit()
+   {
+      gameObject.SetActive(false);
+   }
+}
