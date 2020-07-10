d4861be90f5fdd268c12b15197aece446d06176b
Tom <t.rakoto76@gmail.com>
2020-07-10 14:50:57 +0200

    paramters in gamemanaer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,13 +8,41 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private Updater _updater;
-
+    private float ghostSpeed = 1.0f;
+    private float pacSpeed = 6.0f;
+    private TAccessor<GhostList> _ghosts; 
+    private TAccessor<PacmanList> _pacs; 
+    public float GhostSpeed
+    {
+        get => ghostSpeed;
+        set
+        { 
+            ghostSpeed = value;
+            foreach (var module in _ghosts.GetAllModules())
+            {
+                module.navMeshAgent.speed = ghostSpeed;
+            }
+        }
+    }
+    public float PacSpeed
+    {
+        get => pacSpeed;
+        set
+        { 
+            pacSpeed = value;
+            foreach (var module in _pacs.GetAllModules())
+            {
+                module.navMeshAgent.speed = pacSpeed;
+            }
+        }
+    }
 
     private void Awake()
     {
         Time.timeScale = 1f;
         _updater = Updater.Instance;
         _updater.InitAccessors();
+        _ghosts = TAccessor<GhostList>.Instance;
     }
 
     // Update is called once per frame
--- Assets/Scripts/GhostList.cs
+++ Assets/Scripts/GhostList.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 //list of all ghosts to have an easy reference to all of them
 public class GhostList : MonoBehaviour
 {
+    public NavMeshAgent navMeshAgent;
     private void Awake()
     {
         TAccessor<GhostList>.Instance.AddItem(this);
