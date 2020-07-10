932a82af7ce64c7f685aac7cb4fcf3285dd3f91a
Tom <t.rakoto76@gmail.com>
2020-07-10 16:48:50 +0200

    return to old

--- Assets/Scripts/KillPlayerScriptSystem.cs
+++ Assets/Scripts/KillPlayerScriptSystem.cs
@@ -7,12 +7,16 @@ public class KillPlayerScriptSystem : MonoBehaviour
     //if ghost collides with pacman, kill it
     private void OnCollisionEnter(Collision other)
     {
-        
+        if (!other.collider.CompareTag("Pacman")) return;
+        GetComponent<GhostScore>().Score+=5;
+        Destroy(other.gameObject);
+    /*
+        On a essayez de ne pas utiliser les tags ni les GetComponent mais on a pas réussi
         if (TAccessor<GhostScore>.Instance.GetModuleByRef(other.gameObject) != null)
         {
             GetComponent<GhostScore>().Score+=5;
             Destroy(other.gameObject);
-        }
+        }*/
 
     }
 }
